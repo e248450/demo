288eb3d64aa5f644c7b9d44dd655f391f1dd10d6
e248450 <e248450@sbl.ch>
2022-11-16 14:44:48 +0100

    change color of player

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function handleKeyPress(ev) {
-    if (ev.code === "Space") {
     const_player = document.querySelector("#player")
+    if (ev.code === "Space") {
+    
     player.setAttribute("cx", 100*Math.random())
     player.setAttribute("cy", 100*Math.random())
+}   else if (ev.key === "r") {
+    player.setAttribute ("fill", "red")
 }
 }
 

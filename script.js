e05d191845dead9a19c9a5b152ba5b8ccce24eab
e248450 <e248450@sbl.ch>
2022-11-16 14:42:06 +0100

    only jump on space

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function handleKeyPress(ev) {
+    if (ev.code === "Space") {
     const_player = document.querySelector("#player")
     player.setAttribute("cx", 100*Math.random())
     player.setAttribute("cy", 100*Math.random())
+}
 }
 
 
